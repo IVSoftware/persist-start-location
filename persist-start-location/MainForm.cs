b2b325b8e9238e8c448daf7911bf5770c0819b35
Tom Gregor <tgregor@ivsoftware.com>
2022-12-06 11:39:46 -0700

    Add project files.

---
+++ persist-start-location/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persist_start_location
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            buttonSaveSizeAndPosition.Click += saveSizeAndPosition;
+        }
+
+        private async void saveSizeAndPosition(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Location = JsonConvert.SerializeObject(Location);
+            Properties.Settings.Default.Save();
+            Properties.Settings.Default.Size = JsonConvert.SerializeObject(Size);
+            Properties.Settings.Default.Save();
+            var textB4 = Text;
+            Text = $"Location = {Location} Size = {Size}";
+            await Task.Delay(1000);
+            Text = textB4;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            if(string.IsNullOrWhiteSpace(Properties.Settings.Default.Location))
+            {
+                saveSizeAndPosition(this, EventArgs.Empty);
+            }
+            Location = JsonConvert.DeserializeObject<Point>(Properties.Settings.Default.Location);
+            Size = JsonConvert.DeserializeObject<Size>(Properties.Settings.Default.Size);
+        }
+    }
+}
